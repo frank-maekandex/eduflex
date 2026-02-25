e87f9b7da3d667291bc6d97b152bce1be52c1d20
User <user@Users-MacBook-Air.local>
2026-02-25 12:25:33 +0100

    added

--- app/components/Hero.vue
+++ app/components/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative w-full min-h-[300px] md:min-h-[550px] rounded-3xl md:rounded-5xl overflow-hidden mt-2 mb-28">
+  <div class="relative w-full h-[300px] md:h-[550px] rounded-3xl md:rounded-5xl overflow-hidden mt-2 mb-28">
     <!-- Background image -->
     <NuxtImg
       placeholder
@@ -10,11 +10,11 @@
     />
 
     <!-- Overlay -->
-    <div class="absolute inset-0 bg-gradient-to-r from-primary/95 via-secondary/80 to-primary/40 flex flex-col justify-center px-8 md:px-16">
-      <h1 class="text-white text-3xl md:text-6xl font-bold mb-4">
+    <div class="absolute inset-0 bg-gradient-to-r from-primary/95 via-secondary/80 to-primary/40 flex flex-col justify-center px-6 md:px-16">
+      <h1 class="text-white text-xl md:text-6xl font-bold mb-4">
         Pay School Fees <br /> Flexibly, Not Painfully.
       </h1>
-      <p class="text-white text-xl md:text-2xl mb-6 max-w-[500px]">
+      <p class="text-white text-base md:text-2xl mb-6 max-w-[500px]">
         Pay school fees easily, get instant school payments, and grow trust with lenders.
       </p>
       <NuxtLink to="/onboarding" class="w-fit">
--- app/components/ImageCard.vue
+++ app/components/ImageCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative w-full h-[300px] md:h-[800px] mb-64"> 
+  <div class="relative w-full h-[300px] md:h-[800px] mb-80 md:mb-64"> 
     
     <div class="h-full w-full overflow-hidden rounded-3xl md:rounded-5xl">
       <NuxtImg 
@@ -9,15 +9,15 @@
       />
     </div>
 
-    <div class="absolute right-4 lg:right-10 -bottom-32 flex flex-col items-center p-6 sm:p-0 z-10">
-      <div class="w-full max-w-lg rounded-4xl bg-white p-8 sm:p-12">
-        <h2 class="mb-1 text-4xl font-bold text-black">{{ title }}</h2>
-        <p class="mb-6 text-lg font-medium text-gray-100">{{ subtitle }}</p>
+    <div class="absolute left-0 right-0 lg:left-auto lg:right-10 -bottom-64 md:-bottom-32 flex flex-col items-center px-2 z-10">
+      <div class="w-full max-w-lg rounded-2xl md:rounded-4xl bg-white p-4 md:p-8 sm:p-12">
+        <h2 class="mb-1 text-xl md:text-4xl font-bold text-black">{{ title }}</h2>
+        <p class="mb-6 text-base md:text-lg font-medium text-gray-100">{{ subtitle }}</p>
 
         <ul class="mb-8 space-y-2">
           <li v-for="(feature, index) in features" :key="index" class="flex items-start text-gray-100">
             <span class="mr-3 mt-2 h-1.5 w-1.5 shrink-0 rounded-full bg-gray-100" />
-            <span class="leading-relaxed -mt-0.5">{{ feature }}</span>
+            <span class="leading-relaxed text-sm md:text-base -mt-0.5">{{ feature }}</span>
           </li>
         </ul>
 
